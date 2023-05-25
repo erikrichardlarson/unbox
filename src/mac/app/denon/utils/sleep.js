c6d7ddaf3dfea5ae0b245613dc400fa78b0d612c
erikrichardlarson <erikrichardlarson@gmail.com>
2023-05-24 21:09:05 -0700

    remove old stuff

--- src/mac/app/denon/utils/sleep.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sleep = void 0;
-function sleep(p_ms) {
-    return new Promise((resolve) => setTimeout(resolve, p_ms));
-}
-exports.sleep = sleep;
-//# sourceMappingURL=sleep.js.map
